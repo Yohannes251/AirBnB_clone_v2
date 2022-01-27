daaff07ac518259ecb05a62210e3d4a4edd12dca
Yohannes251 <yohannes.z251@gmail.com>
2022-01-27 18:41:34 +0000

    Template rendering

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+    This script starts a Flask web application
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Homepage"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns page for /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Returns page for /c/<text>"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Returns page for /python/<text>"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """Returns page for /number/<n> when n is an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_temp(n):
+    """Returns a HTML page for /number_template/<n> when n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
