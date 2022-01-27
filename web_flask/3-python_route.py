d158b335114b186a60ea4927b52d5a7f5c268ae5
Yohannes251 <yohannes.z251@gmail.com>
2022-01-27 13:28:23 +0000

    Routing for /python/<text>

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    This script starts a Flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Homepage"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns page for /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Returns page for /c/<text>"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Returns page for /python/<text>"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
